07b1a53b3e28b61e1b7288b3411c1271bdb3dcb6
lesk <les@yoohoomedia.com.au>
2019-05-05 16:29:39 +1000

    initial

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
-})
+               selector   : 'app-home',
+               templateUrl: 'home.page.html',
+               styleUrls  : ['home.page.scss'],
+           })
 export class HomePage {
+    list: string[] = [];
 
+    constructor() {
+        for (let x = 0; x < 50; x++) {
+            this.list.push('' + x);
+        }
+    }
 }
