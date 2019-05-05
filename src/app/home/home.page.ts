9abaeb33a2e2e1b06de70fd039c94cd6a24693e4
lesk <les@yoohoomedia.com.au>
2019-05-05 16:40:25 +1000

    added sliding pages

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,16 +1,44 @@
-import {Component} from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {IonSlides}                    from '@ionic/angular';
 
 @Component({
                selector   : 'app-home',
                templateUrl: 'home.page.html',
                styleUrls  : ['home.page.scss'],
            })
-export class HomePage {
-    list: string[] = [];
+export class HomePage implements OnInit {
+    @ViewChild('slides')
+    slides: IonSlides;
+
+    list1: string[] = [];
+    list2: string[] = [];
 
     constructor() {
         for (let x = 0; x < 50; x++) {
-            this.list.push('' + x);
+            this.list1.push('Page1-' + x);
+            this.list2.push('Page2-' + x);
+        }
+    }
+
+    async ngOnInit() {
+        await this.slides.lockSwipes(true);
+    }
+
+    async onTabChange(tabname: string) {
+        console.log(`tab ${tabname} selected.`);
+        switch (tabname) {
+            case 'page1': {
+                await this.slides.lockSwipes(false);
+                await this.slides.slideTo(0);
+                await this.slides.lockSwipes(true);
+                break;
+            }
+            case 'page2': {
+                await this.slides.lockSwipes(false);
+                await this.slides.slideTo(1);
+                await this.slides.lockSwipes(true);
+                break;
+            }
         }
     }
 }
